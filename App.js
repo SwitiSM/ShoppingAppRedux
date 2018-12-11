c8264c3d07c575aacb0c2d1d2396bb3c790a6a05
SwitiSM <45052890+SwitiSM@users.noreply.github.com>
2018-12-11 22:22:17 +0200

    Add files via upload

---
+++ App.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import Navigator from './src/AppNavigator'
+import LoginForm from './src/screens/LoginForm';
+import {connect,Provider} from 'react-redux'; 
+import {store} from './src/store'
+class App extends React.Component {
+  constructor(props){
+    super(props)
+  }
+  render() {
+    if(this.props.isUserLoggedIn)
+    return (<Navigator />)
+    else 
+    return(<LoginForm />)
+  }
+}
+const mapStateToProps= (state) => {
+  return{
+    isUserLoggedIn: state.isUserLoggedIn,
+  }
+}
+const AppContainer=connect(mapStateToProps)(App)
+
+export default class  ReduxApp extends React.Component{
+  render(){
+    return(
+      <Provider store={store}>
+      <AppContainer />
+      </Provider>
+    )
+  }
+}
